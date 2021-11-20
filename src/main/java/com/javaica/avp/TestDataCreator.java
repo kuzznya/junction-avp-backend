d488f539f9e444c60d755b5bc28521b9318d4875
Ilya Kuznetsov <ikuz2002@gmail.com>
2021-11-20 16:48:07 +0300

    fix: Init data only in dev

--- src/main/java/com/javaica/avp/TestDataCreator.java
+++ src/main/java/com/javaica/avp/TestDataCreator.java
@@ -6,12 +6,14 @@ import com.javaica.avp.service.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@Profile("dev")
 @Order(2)
 @RequiredArgsConstructor
 @Slf4j
