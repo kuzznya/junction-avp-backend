1aa72d3c2df356a3b11c4e2d7088be11bf55cd0a
Ilya Kuznetsov <ikuz2002@gmail.com>
2022-11-20 22:38:13 +0000

    fix: Change backend URL for frontend

--- frontend-admin/openapi-generator.sh
+++ frontend-admin/openapi-generator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
-    -i https://junction.kuzznya.space/v3/api-docs \
+    -i https://junction.kuzznya.com/v3/api-docs \
     -g typescript-axios \
     -o /local/src/api/generated
